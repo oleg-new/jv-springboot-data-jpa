05ac34c600aea4c51031c6d66def8524e17a1d8c
oleg-new <9213195@gmail.com>
2022-09-21 19:22:34 +0300

    extra spaces removed and annotation added @RequiredArgsConstructor into service layer classes

--- src/main/java/mate/academy/springboot/datajpa/service/impl/CategoryServiceImpl.java
+++ src/main/java/mate/academy/springboot/datajpa/service/impl/CategoryServiceImpl.java
@@ -1,17 +1,15 @@
 package mate.academy.springboot.datajpa.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import mate.academy.springboot.datajpa.model.Category;
 import mate.academy.springboot.datajpa.repository.CategoryRepository;
 import mate.academy.springboot.datajpa.service.CategoryService;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
-
-    public CategoryServiceImpl(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
 
     @Override
     public Category save(Category category) {
